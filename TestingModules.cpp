19ee52cd42be1b2c118aa9f3bb95ba90b8f4433c
anonyhostvn <anonyhostvn@gmail.com>
2019-04-16 22:54:15 +0700

    add testting modules

---
+++ TestingModules.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+// #include "modules/DataStructure.h"
+// #include "modules/modules.h"
+// #include "Object/ControlEngine.h"
+// #include "Object/Bullet/Bullet.h"
+
+using namespace std ; 
+
+double pi =  3.14159265358979323846 ; 
+
+// class Motor {
+//     private:
+//         CoordVector<float> Ve ; 
+    
+//     public:
+//         Motor(int Vx , int Vy) {
+//             // Ve.SetCoordX(Vx) ; 
+//             // Ve.SetCoordY(Vy) ; 
+//         }
+
+//         void GetVelocity () {
+//             // printf("%d %d" , Ve.GetCoordX() , Ve.GetCoordY()) ; 
+//         }
+// } ; 
+
+int main () {
+    cout << acos(0.5) << endl ; 
+}
